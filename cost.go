07ef7897f04a3ea0360e23361180621d526d7620
Enrico Rubboli <rubboli@gmail.com>
2015-11-23 12:14:13 +0100

    refactor stuff

--- cost.go
+++ cost.go
@@ -7,17 +7,17 @@ import (
 )
 
 func Cost(x *mat64.Dense, y, theta *mat64.Vector) float64 {
+	//initialize receivers
 	m, _ := x.Dims()
-	_, n := theta.Dims()
-	h := mat64.NewDense(m, n, make([]float64, m*n))
+	h := mat64.NewDense(m, 1, make([]float64, m))
 	squaredErrors := mat64.NewDense(m, 1, make([]float64, m))
 
+	//actual calculus
 	h.Mul(x, theta)
 	squaredErrors.Apply(func(r, c int, v float64) float64 {
 		return math.Pow(h.At(r, c)-y.At(r, c), 2)
-	}, squaredErrors)
+	}, h)
+	j := mat64.Sum(squaredErrors) * 1.0 / (2.0 * float64(m))
 
-	l := 1.0 / (2.0 * float64(m))
-	j := mat64.Sum(squaredErrors) * l
 	return j
 }
