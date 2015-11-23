40b87677b397fd134649a740ec9f57b7b05b76bd
Enrico Rubboli <rubboli@gmail.com>
2015-11-23 03:50:40 +0100

    refactor squaredError

--- cost.go
+++ cost.go
@@ -1,17 +1,23 @@
 package main
 
-import "github.com/gonum/matrix/mat64"
+import (
+	"math"
+
+	"github.com/gonum/matrix/mat64"
+)
 
 func Cost(x *mat64.Dense, y, theta *mat64.Vector) float64 {
-	ar, _ := x.Dims()
-	_, bc := theta.Dims()
-	h := mat64.NewDense(ar, bc, make([]float64, ar*bc))
-	squaredErrors := mat64.NewDense(ar, 1, make([]float64, ar))
+	m, _ := x.Dims()
+	_, n := theta.Dims()
+	h := mat64.NewDense(m, n, make([]float64, m*n))
+	squaredErrors := mat64.NewDense(m, 1, make([]float64, m))
 
 	h.Mul(x, theta)
-	squaredErrors.Sub(h, y)
-	squaredErrors.MulElem(squaredErrors, squaredErrors)
-	l := 1.0 / (2.0 * float64(ar))
+	squaredErrors.Apply(func(r, c int, v float64) float64 {
+		return math.Pow(h.At(r, c)-y.At(r, c), 2)
+	}, squaredErrors)
+
+	l := 1.0 / (2.0 * float64(m))
 	j := mat64.Sum(squaredErrors) * l
 	return j
 }
